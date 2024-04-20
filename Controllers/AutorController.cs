057136db66ac5963a6514dbab909e600cf0e8d3e
Rogério de Sá <rogeriodesaf@gmail.com>
2024-04-20 19:24:07 -0300

    Implememta~ção da Interface Autores

---
+++ Controllers/AutorController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+using WebApi.Services;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutorController : ControllerBase
+    {
+        private readonly IAutorInterface _autorInterface;
+        public AutorController(IAutorInterface autorInterface)
+        {
+            _autorInterface = autorInterface;
+        }   
+
+        [HttpGet("ListarAutores")]
+        public async Task<ActionResult<ResponseModel<List<AutorModel>>>> ListarAutores()
+        {
+            var autores = await _autorInterface.ListarAutores();
+            return Ok(autores);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using WebApi.Data;
+using WebApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IAutorInterface, AutorService>();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
